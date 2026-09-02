91df9c42c01dc298485d7056a9507abe1a70b01a
Alejandro Sierra <alejandro.sierra@hpi.de>
2026-09-02 13:53:29 +0200

    Add evaluation steps to training script for wikimusa dataset

--- train_wikimusa.sh
+++ train_wikimusa.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 echo $HF_HUB_CACHE
+export PYTORCH_NO_CUDA_MEMORY_CACHING=1
 
-python main.py --dataset wikimusa --img_feat image_features.hdf5 --trie_file prefix_tree.pkl --ment_embed_file train_mentions_embeddings.pkl --model_name llama-3-8b --random_seed 42
+python main.py --dataset wikimusa --img_feat image_features.hdf5 --trie_file prefix_tree.pkl --ment_embed_file train_mentions_embeddings.pkl --model_name llama-3-8b  --do_eval_steps 10791 --random_seed 42
+    
